951fc210c9ff60b92352a5f87710b367d6d03af6
MasakazuKinpara <ebipilaf000@gmail.com>
2017-07-19 16:44:28 +0900

    点滅処理作成 キンパラシーン追加

---
+++ Assets/field_team/Scripts/Kinpara/LightController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour {
+    [SerializeField]
+    private float Intensity;
+    private Light m_light;
+
+	// Use this for initialization
+	void Start ( ) {
+        m_light = GetComponent<Light>( );
+	}
+	
+	// Update is called once per frame
+	void Update ( ) {
+		Flashing( );
+        Intensity = m_light.intensity;
+	}
+
+    //点滅処理をこの中で処理する
+    private void Flashing( ) {
+        m_light.intensity = Mathf.Sin( Time.time ) + 0.5f;
+    }
+}
